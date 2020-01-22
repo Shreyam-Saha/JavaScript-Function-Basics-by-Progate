a11db55a17542a66d1b629a37411b77f69837a84
Shreyam Saha <sahashreyam123@gmail.com>
2020-01-22 15:28:31 +0530

    Add files via upload

---
+++ page2/script.js
@@ -0,0 +1,11 @@
+// Assign a function to the hello constant
+const hello = function(){
+  console.log("Hello!");
+  console.log("I am Shreyam");
+}
+
+
+
+
+// Call the function assigned to the hello constant
+hello();
---
+++ page3/script.js
@@ -0,0 +1,10 @@
+// Assign an arrow function to the greet constant
+const greet = () =>{
+  console.log("Hello");
+}
+
+
+
+// Call the greet function
+
+greet();
